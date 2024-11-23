8a795db5f5a30cebd44ec2238b2015403dd0bce2
Ahmed Sabry <39857006+ahmetsabri@users.noreply.github.com>
2024-11-23 03:22:04 +0300

    Languages and init

---
+++ app/Filament/Resources/LanguageResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LanguageResource\Pages;
+use App\Models\Language;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class LanguageResource extends Resource
+{
+    protected static ?string $model = Language::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-language';
+
+    protected static ?string $navigationLabel = 'Diller';
+
+    protected static ?string $modelLabel = 'Dil';
+    protected static ?string $pluralModelLabel = 'Diller';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required()->label('Dil'),
+                TextInput::make('code')->required()->label('Kod')->placeholder('tr'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->label('Dil'),
+                TextColumn::make('code')->label('Kod'),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageLanguages::route('/'),
+        ];
+    }
+}
