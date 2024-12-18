2810b3e05330c3fae4548ae786395782e6384e99
Ahmed Sabry <39857006+ahmetsabri@users.noreply.github.com>
2024-12-18 21:18:46 +0300

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
         Number::useLocale('tr');
         Model::unguard();
         Model::shouldBeStrict();
-        if (app()->runningInConsole()) {
+        if (!app()->runningInConsole()) {
             $supportedLocales = cache('supported_locales') ?: cache()->rememberForever('supported_locales', fn () => Language::where('is_active', true)->get());
             $supportedLocales = cache('supported_locales')?->pluck('code')->map(fn ($val) => strtolower($val))->toArray();
             FilamentTranslateField::defaultLocales($supportedLocales);
