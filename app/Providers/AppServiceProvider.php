d4ab184f783a6196e02afa8b7f1321a7663d2b69
Ahmed Sabry <39857006+ahmetsabri@users.noreply.github.com>
2024-12-18 23:11:14 +0300

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
         Number::useLocale('tr');
         Model::unguard();
         Model::shouldBeStrict();
+
+        \Livewire\Livewire::forceAssetInjection();
+
         if (!app()->runningInConsole()) {
             $supportedLocales = cache('supported_locales') ?: cache()->rememberForever('supported_locales', fn () => Language::where('is_active', true)->get());
             $supportedLocales = cache('supported_locales')?->pluck('code')->map(fn ($val) => strtolower($val))->toArray();
